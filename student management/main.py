3aa7aca08f25f91feb7fc7cbd17670da9cdb2a3d
anees <aneesurrehman423@gmail.com>
2021-11-30 18:36:37 +0530

    sourcecode

---
+++ student management/main.py
@@ -0,0 +1,235 @@
+from flask import Flask,render_template,request,session,redirect,url_for,flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_user,logout_user,login_manager,LoginManager
+from flask_login import login_required,current_user
+import json
+
+# MY db connection
+local_server= True
+app = Flask(__name__)
+app.secret_key='kusumachandashwini'
+
+
+# this is for getting unique user access
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+# app.config['SQLALCHEMY_DATABASE_URL']='mysql://username:password@localhost/databas_table_name'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:@localhost/students'
+db=SQLAlchemy(app)
+
+# here we will create db models that is tables
+class Test(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100))
+    email=db.Column(db.String(100))
+
+class Department(db.Model):
+    cid=db.Column(db.Integer,primary_key=True)
+    branch=db.Column(db.String(100))
+
+class Attendence(db.Model):
+    aid=db.Column(db.Integer,primary_key=True)
+    rollno=db.Column(db.String(100))
+    attendance=db.Column(db.Integer())
+
+class Trig(db.Model):
+    tid=db.Column(db.Integer,primary_key=True)
+    rollno=db.Column(db.String(100))
+    action=db.Column(db.String(100))
+    timestamp=db.Column(db.String(100))
+
+
+class User(UserMixin,db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(50))
+    email=db.Column(db.String(50),unique=True)
+    password=db.Column(db.String(1000))
+
+
+
+
+
+class Student(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    rollno=db.Column(db.String(50))
+    sname=db.Column(db.String(50))
+    sem=db.Column(db.Integer)
+    gender=db.Column(db.String(50))
+    branch=db.Column(db.String(50))
+    email=db.Column(db.String(50))
+    number=db.Column(db.String(12))
+    address=db.Column(db.String(100))
+    
+
+@app.route('/')
+def index(): 
+    return render_template('index.html')
+
+@app.route('/studentdetails')
+def studentdetails():
+    query=db.engine.execute(f"SELECT * FROM `student`") 
+    return render_template('studentdetails.html',query=query)
+
+@app.route('/triggers')
+def triggers():
+    query=db.engine.execute(f"SELECT * FROM `trig`") 
+    return render_template('triggers.html',query=query)
+
+@app.route('/department',methods=['POST','GET'])
+def department():
+    if request.method=="POST":
+        dept=request.form.get('dept')
+        query=Department.query.filter_by(branch=dept).first()
+        if query:
+            flash("Department Already Exist","warning")
+            return redirect('/department')
+        dep=Department(branch=dept)
+        db.session.add(dep)
+        db.session.commit()
+        flash("Department Addes","success")
+    return render_template('department.html')
+
+@app.route('/addattendance',methods=['POST','GET'])
+def addattendance():
+    query=db.engine.execute(f"SELECT * FROM `student`") 
+    if request.method=="POST":
+        rollno=request.form.get('rollno')
+        attend=request.form.get('attend')
+        print(attend,rollno)
+        atte=Attendence(rollno=rollno,attendance=attend)
+        db.session.add(atte)
+        db.session.commit()
+        flash("Attendance added","warning")
+
+        
+    return render_template('attendance.html',query=query)
+
+@app.route('/search',methods=['POST','GET'])
+def search():
+    if request.method=="POST":
+        rollno=request.form.get('roll')
+        bio=Student.query.filter_by(rollno=rollno).first()
+        attend=Attendence.query.filter_by(rollno=rollno).first()
+        return render_template('search.html',bio=bio,attend=attend)
+        
+    return render_template('search.html')
+
+@app.route("/delete/<string:id>",methods=['POST','GET'])
+@login_required
+def delete(id):
+    db.engine.execute(f"DELETE FROM `student` WHERE `student`.`id`={id}")
+    flash("Slot Deleted Successful","danger")
+    return redirect('/studentdetails')
+
+
+@app.route("/edit/<string:id>",methods=['POST','GET'])
+@login_required
+def edit(id):
+    dept=db.engine.execute("SELECT * FROM `department`")
+    posts=Student.query.filter_by(id=id).first()
+    if request.method=="POST":
+        rollno=request.form.get('rollno')
+        sname=request.form.get('sname')
+        sem=request.form.get('sem')
+        gender=request.form.get('gender')
+        branch=request.form.get('branch')
+        email=request.form.get('email')
+        num=request.form.get('num')
+        address=request.form.get('address')
+        query=db.engine.execute(f"UPDATE `student` SET `rollno`='{rollno}',`sname`='{sname}',`sem`='{sem}',`gender`='{gender}',`branch`='{branch}',`email`='{email}',`number`='{num}',`address`='{address}'")
+        flash("Slot is Updates","success")
+        return redirect('/studentdetails')
+    
+    return render_template('edit.html',posts=posts,dept=dept)
+
+
+@app.route('/signup',methods=['POST','GET'])
+def signup():
+    if request.method == "POST":
+        username=request.form.get('username')
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=User.query.filter_by(email=email).first()
+        if user:
+            flash("Email Already Exist","warning")
+            return render_template('/signup.html')
+        encpassword=generate_password_hash(password)
+
+        new_user=db.engine.execute(f"INSERT INTO `user` (`username`,`email`,`password`) VALUES ('{username}','{email}','{encpassword}')")
+
+        # this is method 2 to save data in db
+        # newuser=User(username=username,email=email,password=encpassword)
+        # db.session.add(newuser)
+        # db.session.commit()
+        flash("Signup Succes Please Login","success")
+        return render_template('login.html')
+
+          
+
+    return render_template('signup.html')
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method == "POST":
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password,password):
+            login_user(user)
+            flash("Login Success","primary")
+            return redirect(url_for('index'))
+        else:
+            flash("invalid credentials","danger")
+            return render_template('login.html')    
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Logout SuccessFul","warning")
+    return redirect(url_for('login'))
+
+
+
+@app.route('/addstudent',methods=['POST','GET'])
+@login_required
+def addstudent():
+    dept=db.engine.execute("SELECT * FROM `department`")
+    if request.method=="POST":
+        rollno=request.form.get('rollno')
+        sname=request.form.get('sname')
+        sem=request.form.get('sem')
+        gender=request.form.get('gender')
+        branch=request.form.get('branch')
+        email=request.form.get('email')
+        num=request.form.get('num')
+        address=request.form.get('address')
+        query=db.engine.execute(f"INSERT INTO `student` (`rollno`,`sname`,`sem`,`gender`,`branch`,`email`,`number`,`address`) VALUES ('{rollno}','{sname}','{sem}','{gender}','{branch}','{email}','{num}','{address}')")
+    
+
+        flash("Booking Confirmed","info")
+
+
+    return render_template('student.html',dept=dept)
+@app.route('/test')
+def test():
+    try:
+        Test.query.all()
+        return 'My database is Connected'
+    except:
+        return 'My db is not Connected'
+
+
+app.run(debug=True)    
---
+++ student management/students.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 11, 2021 at 01:03 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `students`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendence`
+--
+
+CREATE TABLE `attendence` (
+  `aid` int(11) NOT NULL,
+  `rollno` varchar(20) NOT NULL,
+  `attendance` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attendence`
+--
+
+INSERT INTO `attendence` (`aid`, `rollno`, `attendance`) VALUES
+(6, '1ve17cs012', 98);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `cid` int(11) NOT NULL,
+  `branch` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`cid`, `branch`) VALUES
+(2, 'Information Science'),
+(3, 'Electronic and Communication'),
+(4, 'Electrical & Electronic'),
+(5, 'Civil '),
+(7, 'computer science'),
+(8, 'IOT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `rollno` varchar(20) NOT NULL,
+  `sname` varchar(50) NOT NULL,
+  `sem` int(20) NOT NULL,
+  `gender` varchar(50) NOT NULL,
+  `branch` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `number` varchar(12) NOT NULL,
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Triggers `student`
+--
+DELIMITER $$
+CREATE TRIGGER `DELETE` BEFORE DELETE ON `student` FOR EACH ROW INSERT INTO trig VALUES(null,OLD.rollno,'STUDENT DELETED',NOW())
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `Insert` AFTER INSERT ON `student` FOR EACH ROW INSERT INTO trig VALUES(null,NEW.rollno,'STUDENT INSERTED',NOW())
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `UPDATE` AFTER UPDATE ON `student` FOR EACH ROW INSERT INTO trig VALUES(null,NEW.rollno,'STUDENT UPDATED',NOW())
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL,
+  `name` varchar(52) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `name`, `email`) VALUES
+(1, 'aaa', 'aaa@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trig`
+--
+
+CREATE TABLE `trig` (
+  `tid` int(11) NOT NULL,
+  `rollno` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `timestamp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `trig`
+--
+
+INSERT INTO `trig` (`tid`, `rollno`, `action`, `timestamp`) VALUES
+(7, '1ve17cs012', 'STUDENT INSERTED', '2021-01-10 19:19:56'),
+(8, '1ve17cs012', 'STUDENT UPDATED', '2021-01-10 19:20:31'),
+(9, '1ve17cs012', 'STUDENT DELETED', '2021-01-10 19:21:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`) VALUES
+(4, 'anees', 'anees@gmail.com', 'pbkdf2:sha256:150000$1CSLss89$ef995dfc48121768b2070bfbe7a568871cd56fac85ac7c95a1e645c8806146e9');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendence`
+--
+ALTER TABLE `attendence`
+  ADD PRIMARY KEY (`aid`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trig`
+--
+ALTER TABLE `trig`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendence`
+--
+ALTER TABLE `attendence`
+  MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `trig`
+--
+ALTER TABLE `trig`
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
